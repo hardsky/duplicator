063817e67e06c10a2c351ac7a023887a08d5f335
Nikolay Mihailov <hardsky@yandex.ru>
2018-08-12 18:06:55 +0300

    add redis cache

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/go-pg/pg"
+	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 )
 
@@ -35,6 +37,14 @@ func handleDuplicate(w http.ResponseWriter, r *http.Request) {
 	user1 := vars["userId1"]
 	user2 := vars["userId2"]
 
+	val := rd.Get(fmt.Sprintf("%s,%s", user1, user2)).Val()
+	if val == "true" {
+		fmt.Println("cache matched")
+		writeTrue(w)
+		return
+	}
+	fmt.Println("cache missed")
+
 	var count int
 	db.QueryOne(pg.Scan(&count), `SELECT COUNT (1) FROM
 (SELECT ip_addr FROM conn_log
@@ -44,17 +54,36 @@ INNER JOIN
         WHERE user_id = ?) c2
 ON c1.ip_addr = c2.ip_addr
 `, user1, user2)
+
+	if count > 2 {
+		go func() {
+			rd.Set(fmt.Sprintf("%s,%s", user1, user2), "true", 0).Err()
+			rd.Set(fmt.Sprintf("%s,%s", user2, user1), "true", 0).Err()
+		}()
+	}
+
 	res := &DuplicateResponse{count >= 2}
 	b, _ := json.Marshal(res)
 	w.Write(b)
 }
 
+func writeTrue(w http.ResponseWriter) {
+	b, _ := json.Marshal(&DuplicateResponse{true})
+	w.Write(b)
+}
+
 var db *pg.DB
+var rd *redis.Client
 
 func init() {
 	db = pg.Connect(&pg.Options{
 		User:     "postgres",
 		Password: "jQnas3wed",
 		Database: "duplicator",
+	})
+	rd = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
 	})
 }
